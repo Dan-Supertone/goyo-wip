2626664d20d4621e139c4523c594b669e1646db3
dan@supertone.ai <dan@supertone.ai>
2022-10-20 17:19:44 +0900

    Update knob-test

--- assets/scripts/dist/player.dev.js
+++ assets/scripts/dist/player.dev.js
@@ -2,31 +2,42 @@
 
 // 오디오 파일 불러오기 & 배열화
 var audioAA = new Audio('../assets/audios/audioAA.mp3');
-audioAA.loop = true;
 var audioAB = new Audio('../assets/audios/audioAB.mp3');
-audioAB.loop = true;
 var audioAC = new Audio('../assets/audios/audioAC.mp3');
-audioAC.loop = true;
 var audioBA = new Audio('../assets/audios/audioBA.mp3');
-audioBA.loop = true;
 var audioBB = new Audio('../assets/audios/audioBB.mp3');
-audioBB.loop = true;
 var audioBC = new Audio('../assets/audios/audioBC.mp3');
-audioBC.loop = true;
+var audioCA = new Audio('../assets/audios/audioCA.mp3');
+var audioCB = new Audio('../assets/audios/audioCB.mp3');
+var audioCC = new Audio('../assets/audios/audioCC.mp3');
+var audioDA = new Audio('../assets/audios/audioDA.mp3');
+var audioDB = new Audio('../assets/audios/audioDB.mp3');
+var audioDC = new Audio('../assets/audios/audioDC.mp3');
 var audioPack = {
   0: ['Subway Station', audioAA, audioAB, audioAC],
-  1: ['Boomy Room', audioBA, audioBB, audioBC]
+  1: ['Boomy Room', audioBA, audioBB, audioBC],
+  2: ['Outdoors Speech', audioCA, audioCB, audioCC],
+  3: ['Noisy Radio', audioDA, audioDB, audioDC]
 };
 var audioCount = Object.keys(audioPack).length - 1;
+var btnPlay = document.getElementById('btn-play');
+var btnPause = document.getElementById('btn-pause');
+var btnPrev = document.getElementById('btn-prev');
+var btnNext = document.getElementById('btn-next');
+var navPlay = document.getElementById('nav-play');
 var nowPlaying = 0;
-var isPlaying = false; // 방문자 기기 체크 (iPhone, iPad, iOS)
+var isPlaying = false; // 노브 조작 마우스 세로 드래그만 되게!!!!!
+// 방문자 기기 체크 (iPhone, iPad, iOS)
 // 모바일인 경우, iPad인 경우
 // 창 너비에 변화가 일어날 때
 // 재생 상황 리셋
 // 플레이어 네비게이션 타이틀 리셋
 
-var btnPrev = document.getElementById('btn-prev');
-var btnNext = document.getElementById('btn-next');
+navPlay.innerHTML = audioPack[nowPlaying][0]; // 볼륨 값 리셋 temp
+
+document.getElementById('value-a').innerHTML = document.getElementById('knob-a').value;
+document.getElementById('value-b').innerHTML = document.getElementById('knob-b').value;
+document.getElementById('value-c').innerHTML = document.getElementById('knob-c').value;
 btnPrev.addEventListener('click', function () {
   pauseAll();
 
@@ -36,8 +47,9 @@ btnPrev.addEventListener('click', function () {
     nowPlaying = audioCount;
   }
 
-  ; // playerNav.html(audioPack[nowPlaying][0]);
-  // setVolume();
+  ;
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  setVolume();
 });
 btnNext.addEventListener('click', function () {
   pauseAll();
@@ -49,10 +61,9 @@ btnNext.addEventListener('click', function () {
   }
 
   ;
-}); // 플레이어
-
-var btnPlay = document.getElementById('btn-play');
-var btnPause = document.getElementById('btn-pause');
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  setVolume();
+});
 btnPlay.addEventListener('click', play);
 btnPause.addEventListener('click', pause);
 
@@ -60,6 +71,7 @@ function play() {
   audioPack[nowPlaying][1].play();
   audioPack[nowPlaying][2].play();
   audioPack[nowPlaying][3].play();
+  progress();
 }
 
 ;
@@ -90,7 +102,56 @@ function pauseAll() {
 
 function controlA(a) {
   document.getElementById('value-a').innerHTML = a;
-  audioAA.volume = a;
+  audioPack[nowPlaying][1].volume = a;
 }
 
 ;
+
+function controlB(a) {
+  document.getElementById('value-b').innerHTML = a;
+  audioPack[nowPlaying][2].volume = a;
+}
+
+;
+
+function controlC(a) {
+  document.getElementById('value-c').innerHTML = a;
+  audioPack[nowPlaying][3].volume = a;
+}
+
+;
+
+function setVolume() {
+  audioPack[nowPlaying][1].volume = document.getElementById('knob-a').value;
+  audioPack[nowPlaying][2].volume = document.getElementById('knob-b').value;
+  audioPack[nowPlaying][3].volume = document.getElementById('knob-c').value;
+}
+
+;
+
+function progress() {
+  audioPack[nowPlaying][1].addEventListener("timeupdate", function () {
+    var duration = audioPack[nowPlaying][1].duration;
+    var currentTime = audioPack[nowPlaying][1].currentTime;
+    var currentPercent = currentTime * 100 / duration;
+    var progress = document.getElementById('progress-bar');
+    progress.style.width = currentPercent + '%';
+  });
+}
+
+;
+
+audioPack[nowPlaying][1].onended = function () {
+  pauseAll();
+
+  if (nowPlaying < audioCount) {
+    nowPlaying++;
+  } else {
+    nowPlaying = 0;
+  }
+
+  ;
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  play();
+  setVolume();
+};
--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -1,28 +1,31 @@
 // 오디오 파일 불러오기 & 배열화
 const audioAA = new Audio('../assets/audios/audioAA.mp3');
-audioAA.loop = true;
-
 const audioAB = new Audio('../assets/audios/audioAB.mp3');
-audioAB.loop = true;
-
 const audioAC = new Audio('../assets/audios/audioAC.mp3');
-audioAC.loop = true;
-
 const audioBA = new Audio('../assets/audios/audioBA.mp3');
-audioBA.loop = true;
-
 const audioBB = new Audio('../assets/audios/audioBB.mp3');
-audioBB.loop = true;
-
 const audioBC = new Audio('../assets/audios/audioBC.mp3');
-audioBC.loop = true;
+const audioCA = new Audio('../assets/audios/audioCA.mp3');
+const audioCB = new Audio('../assets/audios/audioCB.mp3');
+const audioCC = new Audio('../assets/audios/audioCC.mp3');
+const audioDA = new Audio('../assets/audios/audioDA.mp3');
+const audioDB = new Audio('../assets/audios/audioDB.mp3');
+const audioDC = new Audio('../assets/audios/audioDC.mp3');
 
 const audioPack = {
   0: ['Subway Station', audioAA, audioAB, audioAC],
-  1: ['Boomy Room', audioBA, audioBB, audioBC]
+  1: ['Boomy Room', audioBA, audioBB, audioBC],
+  2: ['Outdoors Speech', audioCA, audioCB, audioCC],
+  3: ['Noisy Radio', audioDA, audioDB, audioDC]
 };
+
 const audioCount = Object.keys(audioPack).length - 1;
 
+const btnPlay = document.getElementById('btn-play');
+const btnPause = document.getElementById('btn-pause');
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+const navPlay = document.getElementById('nav-play');
 
 let nowPlaying = 0;
 let isPlaying = false;
@@ -33,6 +36,9 @@ let isPlaying = false;
 
 
 
+// 노브 조작 마우스 세로 드래그만 되게!!!!!
+
+
 
 // 방문자 기기 체크 (iPhone, iPad, iOS)
   // 모바일인 경우, iPad인 경우
@@ -40,9 +46,23 @@ let isPlaying = false;
   // 재생 상황 리셋
 
 // 플레이어 네비게이션 타이틀 리셋
+navPlay.innerHTML = audioPack[nowPlaying][0];
 
-const btnPrev = document.getElementById('btn-prev');
-const btnNext = document.getElementById('btn-next');
+// 볼륨 값 리셋 temp
+document.getElementById('value-a').innerHTML = document.getElementById('knob-a').value;
+document.getElementById('value-b').innerHTML = document.getElementById('knob-b').value;
+document.getElementById('value-c').innerHTML = document.getElementById('knob-c').value;
+
+
+
+
+
+
+
+
+
+
+
 
 btnPrev.addEventListener('click', function() {
   pauseAll();
@@ -51,8 +71,8 @@ btnPrev.addEventListener('click', function() {
   } else {
     nowPlaying = audioCount;
   };
-  // playerNav.html(audioPack[nowPlaying][0]);
-  // setVolume();
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  setVolume();
 });
 
 btnNext.addEventListener('click', function() {
@@ -62,15 +82,13 @@ btnNext.addEventListener('click', function() {
   } else {
     nowPlaying = 0;
   };
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  setVolume();
 });
 
 
 
 
-// 플레이어
-
-const btnPlay = document.getElementById('btn-play');
-const btnPause = document.getElementById('btn-pause');
 
 btnPlay.addEventListener('click', play);
 btnPause.addEventListener('click', pause);
@@ -82,6 +100,7 @@ function play() {
   audioPack[nowPlaying][1].play();
   audioPack[nowPlaying][2].play();
   audioPack[nowPlaying][3].play();
+  progress();
 };
 
 function pause() {
@@ -105,7 +124,62 @@ function pauseAll() {
 
 
 
+
+
+
+
+
+
+
+
 function controlA(a) {
   document.getElementById('value-a').innerHTML = a;
-  audioAA.volume = a;
+  audioPack[nowPlaying][1].volume = a;
 };
+
+function controlB(a) {
+  document.getElementById('value-b').innerHTML = a;
+  audioPack[nowPlaying][2].volume = a;
+};
+
+function controlC(a) {
+  document.getElementById('value-c').innerHTML = a;
+  audioPack[nowPlaying][3].volume = a;
+};
+
+function setVolume() {
+  audioPack[nowPlaying][1].volume = document.getElementById('knob-a').value;
+  audioPack[nowPlaying][2].volume = document.getElementById('knob-b').value;
+  audioPack[nowPlaying][3].volume = document.getElementById('knob-c').value;
+};
+
+
+
+
+
+
+
+function progress() {
+  audioPack[nowPlaying][1].addEventListener("timeupdate", function () {
+    let duration = audioPack[nowPlaying][1].duration;
+    let currentTime = audioPack[nowPlaying][1].currentTime;
+    let currentPercent = currentTime * 100 / duration;
+    let progress = document.getElementById('progress-bar');
+    progress.style.width = currentPercent + '%';
+  });
+};
+
+audioPack[nowPlaying][1].onended = function() {
+  pauseAll();
+  if (nowPlaying < audioCount) {
+    nowPlaying++;
+  } else {
+    nowPlaying = 0;
+  };
+  navPlay.innerHTML = audioPack[nowPlaying][0];
+  play();
+  setVolume();
+};
+
+
+
